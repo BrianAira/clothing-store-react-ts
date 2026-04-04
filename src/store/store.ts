425a5c5677d6c7e0ab6f158f8460ccfd12f57149
Brian Aira <136931601+BrianAira@users.noreply.github.com>
2026-04-03 22:00:36 -0300

    feat: definicion de estado global utilizando libreria React Redux

---
+++ src/store/store.ts
@@ -0,0 +1,18 @@
+
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../features/Auth/redux/authSlice";
+import cartReducer from "../features/Cart/redux/cartSlice";
+
+
+// const persistedToken = localStorage.getItem("token");
+// const persistedUser = localStorage.getItem("user");
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    cart: cartReducer,
+  },
+ 
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
