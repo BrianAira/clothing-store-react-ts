e8a4ff2d75e2da36e1b5b0eb86e3f6175f8f6958
Brian Aira <136931601+BrianAira@users.noreply.github.com>
2026-04-03 21:53:28 -0300

    feat: exportar configuracion de heroUI para procesar los estilos en tailwind

---
+++ src/hero.ts
@@ -0,0 +1,7 @@
+import { heroui } from "@heroui/react";
+
+/**
+ * Este archivo exporta la configuración de HeroUI 
+ * para que Tailwind 4 pueda procesar los temas y colores.
+ */
+export default heroui();
