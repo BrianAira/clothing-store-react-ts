7cc3715a41db865151ab0a47a3ed474b3a60b976
Brian Aira <136931601+BrianAira@users.noreply.github.com>
2026-04-15 22:33:06 -0300

    feat(api):definir traduccion de datos de api

---
+++ src/features/Orders/api/mappers.ts
@@ -0,0 +1,28 @@
+import type { IOrder, IOrderItem, OrderApi, OrderItemApi } from "../types/Order";
+
+export function toOrderItem(api: OrderItemApi): IOrderItem {
+    return {
+        productId: api.product_id,
+        quantity: api.quantity,
+        price: api.unit_price,
+        product: {
+            id: api.product?.id,
+            name: api.product?.name || "Producto Desconocido",
+            imageUrl: api.product?.image_url || "",
+        }
+    };
+}
+
+export function toOrder(api: OrderApi): IOrder {
+    return {
+        id: api.id,
+        userId: api.user_id,
+        orderDate: api.order_date,
+        status: api.status,
+
+        shippingAddress: api.shipping_address_snapshot||"sin direccion",
+        // locality: api.locality,
+        total: typeof api.total_amount==="string"?parseFloat(api.total_amount): api.total_amount,
+        items: (api.items||[]).map(toOrderItem),
+    };
+}
